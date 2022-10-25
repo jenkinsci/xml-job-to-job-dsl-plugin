8bb6be39ab53dff338030b01e86b5a0f140bd31c
Natalia Woodson <nwoodson@slack-corp.com>
2022-10-25 12:55:38 -0400

    add custom property and log rotator properties

---
+++ src/main/java/com/adq/jenkins/xmljobtodsl/dsl/strategies/custom/DSLIntStrategy.java
@@ -0,0 +1,26 @@
+package com.adq.jenkins.xmljobtodsl.dsl.strategies.custom;
+
+import com.adq.jenkins.xmljobtodsl.dsl.strategies.DSLMethodStrategy;
+import com.adq.jenkins.xmljobtodsl.parsers.PropertyDescriptor;
+
+import static java.lang.Integer.parseInt;
+
+public class DSLIntStrategy extends DSLMethodStrategy {
+
+    private final String methodName;
+
+    public DSLIntStrategy(int tabs, PropertyDescriptor propertyDescriptor, String methodName) {
+        super(tabs, propertyDescriptor, methodName);
+        this.methodName = methodName;
+    }
+
+
+    @Override
+    public String toDSL() {
+        PropertyDescriptor descriptor = (PropertyDescriptor) getDescriptor();
+        String value = descriptor.getValue();
+        return replaceTabs(String.format(getSyntax("syntax.method_call"),
+                methodName, value), getTabs());
+    }
+
+}
