7e43e19e6e82ae738cb43ce36171ac70b84258db
Natalia Woodson <nwoodson@slack-corp.com>
2022-11-02 15:27:59 -0400

    removing pre from ignored and adding back to translator.properties

--- src/main/java/com/adq/jenkins/xmljobtodsl/dsl/strategies/AbstractDSLStrategy.java
+++ src/main/java/com/adq/jenkins/xmljobtodsl/dsl/strategies/AbstractDSLStrategy.java
@@ -56,7 +56,6 @@ public abstract class AbstractDSLStrategy implements DSLStrategy {
 		propertiesToBeSkipped.add("hudson.plugins.git.extensions.impl.PathRestriction");
 		propertiesToBeSkipped.add("unstableReturn");
 		propertiesToBeSkipped.add("ignoreMissing");
-		propertiesToBeSkipped.add("pre");
 
 		try {
 			initProperties();
