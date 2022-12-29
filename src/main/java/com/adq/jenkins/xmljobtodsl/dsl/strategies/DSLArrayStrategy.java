3e031ce262fd9134ad0c560eb57473a1b5a66d61
Pamela Gluss <pgluss@slack-corp.com>
2022-12-29 12:45:01 -0800

    now both ways work

--- src/main/java/com/adq/jenkins/xmljobtodsl/dsl/strategies/DSLArrayStrategy.java
+++ src/main/java/com/adq/jenkins/xmljobtodsl/dsl/strategies/DSLArrayStrategy.java
@@ -4,16 +4,20 @@ import com.adq.jenkins.xmljobtodsl.parsers.PropertyDescriptor;
 
 public class DSLArrayStrategy extends DSLMethodStrategy implements IValueStrategy {
 	private String methodName;
+	private String parentType;
 	public DSLArrayStrategy(int tabs, PropertyDescriptor propertyDescriptor, String methodName) {
 		super(tabs, propertyDescriptor, methodName);
 		this.methodName = methodName;
 		this.setTabs(tabs);
+
+		parentType = getType(propertyDescriptor.getParent());
 	}
 
 	@Override
 	public String toDSL() {
 		String childrenDSL = getChildrenDSL();
-		if (!childrenDSL.isEmpty()) {
+		// Assume if the parent is an object that this is likely supposed to be a method as opposed to a parameter
+		if (parentType.equals("OBJECT")) {
 			return replaceTabs(String.format(getSyntax("syntax.method_call"),
 					methodName, String.format("[%s]", childrenDSL)), getTabs());
 		} else {
