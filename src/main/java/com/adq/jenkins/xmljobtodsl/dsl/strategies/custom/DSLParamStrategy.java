1896ead7c31591095b25c8f7f63424778ee5b093
Natalia Woodson <nwoodson@slack-corp.com>
2023-01-03 19:49:37 -0500

    return string params in correct order

--- src/main/java/com/adq/jenkins/xmljobtodsl/dsl/strategies/custom/DSLParamStrategy.java
+++ src/main/java/com/adq/jenkins/xmljobtodsl/dsl/strategies/custom/DSLParamStrategy.java
@@ -4,6 +4,8 @@ import com.adq.jenkins.xmljobtodsl.dsl.strategies.DSLMethodStrategy;
 import com.adq.jenkins.xmljobtodsl.dsl.strategies.DSLStrategy;
 import com.adq.jenkins.xmljobtodsl.parsers.PropertyDescriptor;
 
+import java.util.List;
+
 public class DSLParamStrategy extends DSLMethodStrategy {
 
 	private final String methodName;
@@ -21,8 +23,14 @@ public class DSLParamStrategy extends DSLMethodStrategy {
 
 	public String getOrderedChildrenDSL() {
 		PropertyDescriptor propertyDescriptor = (PropertyDescriptor) getDescriptor();
-		if (propertyDescriptor.getName().equals("hudson.model.StringParameterDefinition")) {
-			String defaultValue = getChildrenByName("defaultValue").toDSL();
+		if (propertyDescriptor.getName().equals("hudson.model.StringParameterDefinition") || propertyDescriptor.getName().equals("hudson.model.BooleanParameterDefinition")) {
+			String defaultValue = "\"\"";
+			List <PropertyDescriptor> children = propertyDescriptor.getProperties();
+			for (PropertyDescriptor child : children ) {
+				if (child.getName().equals("defaultValue")) {
+					defaultValue = getChildrenByName("defaultValue").toDSL();
+				}
+			}
 			String name = getChildrenByName("name").toDSL();
 			String description = getChildrenByName("description").toDSL();
 			return name + ", " + defaultValue + ", " + description;
